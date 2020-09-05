73cf619afaf617fbf5d578be4c71da3c082a22df
FE <fabian.erdel@ibcg.biotoul.fr>
2020-09-05 14:11:03 +0200

    update

--- sample-scripts/plot_frap_functions.R
+++ sample-scripts/plot_frap_functions.R
@@ -2,7 +2,7 @@
 library(frappe)
 
 ## define plot range
-time <- 10^seq(-4,1.5,by=0.1)
+time <- 10^seq(-4, 1.5, by=0.1)
 
 ## define parameters for plotted curves
 dcoeff <- 0.1 # diffusion coefficient (um^2/s)
@@ -37,23 +37,23 @@ lines(time, nonbleached_boundary_half_fit(time)(d=dcoeff, h= 10, radius=r), col=
 #
 # full-circle FRAP (reaction-diffusion without boundary)
 plot (time, circle_no_boundary_full_d_fit(time)(d=dcoeff, rc=r, rl=10*r), type="l", lty=2, ylim=c(0,1), xlab="Time (s)", ylab="Norm. Intensity", main="Reaction-Diffusion (binding to gel)\nfull-circle")
-lines(time, circle_no_boundary_full_rd_fit(time)(d=dcoeff, rc=r, rl=10*r, kon=5,   koff=1), col="blue")
-lines(time, circle_no_boundary_full_rd_fit(time)(d=dcoeff, rc=r, rl=10*r, kon=50,  koff=1), col="green")
-lines(time, circle_no_boundary_full_rd_fit(time)(d=dcoeff, rc=r, rl=10*r, kon=500, koff=1), col="red")
+lines(time, circle_no_boundary_full_rd_fit(time)(d=dcoeff, rc=r, rl=10*r, kon=5,    koff=1), col="blue")
+lines(time, circle_no_boundary_full_rd_fit(time)(d=dcoeff, rc=r, rl=10*r, kon=50,   koff=1), col="green")
+lines(time, circle_no_boundary_full_rd_fit(time)(d=dcoeff, rc=r, rl=10*r, kon=500,  koff=1), col="red")
 lines(time, circle_no_boundary_full_rd_fit(time)(d=dcoeff, rc=r, rl=10*r, kon=5000, koff=1), col="gray")
 
 # half-circle FRAP (reaction-diffusion without boundary)
 plot (time, bleached_no_boundary_half_d_fit(time)(d=dcoeff, rc=r, rl=10*r), type="l", lty=2, ylim=c(0,1), xlab="Time (s)", ylab="Norm. Intensity", main="Reaction-Diffusion (binding to gel)\nfull-circle")
-lines(time, bleached_no_boundary_half_rd_fit(time)(d=dcoeff, rc=r, rl=10*r, kon=5,   koff=1), col="blue")
-lines(time, bleached_no_boundary_half_rd_fit(time)(d=dcoeff, rc=r, rl=10*r, kon=50,  koff=1), col="green")
-lines(time, bleached_no_boundary_half_rd_fit(time)(d=dcoeff, rc=r, rl=10*r, kon=500, koff=1), col="red")
+lines(time, bleached_no_boundary_half_rd_fit(time)(d=dcoeff, rc=r, rl=10*r, kon=5,    koff=1), col="blue")
+lines(time, bleached_no_boundary_half_rd_fit(time)(d=dcoeff, rc=r, rl=10*r, kon=50,   koff=1), col="green")
+lines(time, bleached_no_boundary_half_rd_fit(time)(d=dcoeff, rc=r, rl=10*r, kon=500,  koff=1), col="red")
 lines(time, bleached_no_boundary_half_rd_fit(time)(d=dcoeff, rc=r, rl=10*r, kon=5000, koff=1), col="gray")
 
 # half-circle FRAP (reaction-diffusion without boundary)
 plot (time, nonbleached_no_boundary_half_d_fit(time)(d=dcoeff, rc=r, rl=10*r), type="l", lty=2, ylim=c(0.75,1), xlab="Time (s)", ylab="Norm. Intensity", main="Reaction-Diffusion (binding to gel)\nfull-circle")
-lines(time, nonbleached_no_boundary_half_rd_fit(time)(d=dcoeff, rc=r, rl=10*r, kon=5,   koff=1), col="blue")
-lines(time, nonbleached_no_boundary_half_rd_fit(time)(d=dcoeff, rc=r, rl=10*r, kon=50,  koff=1), col="green")
-lines(time, nonbleached_no_boundary_half_rd_fit(time)(d=dcoeff, rc=r, rl=10*r, kon=500, koff=1), col="red")
+lines(time, nonbleached_no_boundary_half_rd_fit(time)(d=dcoeff, rc=r, rl=10*r, kon=5,    koff=1), col="blue")
+lines(time, nonbleached_no_boundary_half_rd_fit(time)(d=dcoeff, rc=r, rl=10*r, kon=50,   koff=1), col="green")
+lines(time, nonbleached_no_boundary_half_rd_fit(time)(d=dcoeff, rc=r, rl=10*r, kon=500,  koff=1), col="red")
 lines(time, nonbleached_no_boundary_half_rd_fit(time)(d=dcoeff, rc=r, rl=10*r, kon=5000, koff=1), col="gray")
 
 
